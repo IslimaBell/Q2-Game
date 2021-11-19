6cb43e3cf4d1b8805c5118848cd05b07a6d83855
MUKILTEO\1710152 <Riley05Park@gmail.com>
2021-11-19 12:18:28 -0800

    Added Size Change Prototype and some other stuff

---
+++ Assets/Riley/SizeChangeScriptPrototype.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeChangeScriptPrototype : MonoBehaviour
+{
+    public float size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(-size, size, 0);
+
+        // increase and decrease size
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            size += 0.1f;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            size -= 0.1f;
+        }
+    }
+}
