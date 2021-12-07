d3cd4efa29069ef9647422a866bfb11f5fff1b97
MUKILTEO\1709546 <Isaiah2124.ib@gmail.com>
2021-12-07 12:42:42 -0800

    Button added

--- Assets/Isaiah/Script/Button.cs
+++ Assets/Isaiah/Script/Button.cs
@@ -9,13 +9,25 @@ public class Button : MonoBehaviour
     GameObject door;
 
     bool isOpened = false;
+    bool isClosed = true;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!isOpened)
         {
             isOpened = true;
+            isClosed = false;
             door.transform.position += new Vector3(0, 4, 0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!isClosed)
+        {
+            isOpened = false;
+            isClosed = true;
+            door.transform.position -= new Vector3(0, 4, 0);
         }
     }
 
