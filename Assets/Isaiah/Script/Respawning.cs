5e3fc356f3072520bfdc1c8257925d53b85d3ef6
MUKILTEO\1709546 <Isaiah2124.ib@gmail.com>
2021-12-09 13:13:10 -0800

    Respawn

---
+++ Assets/Isaiah/Script/Respawning.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respawning : MonoBehaviour
+{
+
+    void Start()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+
+}
