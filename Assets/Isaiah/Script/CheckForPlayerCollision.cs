2e2d5cdf27dcdf459b5866e0022fa1b3ad3555a0
MUKILTEO\1709546 <Isaiah2124.ib@gmail.com>
2021-12-10 11:43:10 -0800

    Enemy kills

--- Assets/Isaiah/Script/CheckForPlayerCollision.cs
+++ Assets/Isaiah/Script/CheckForPlayerCollision.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CheckForPlayerCollision : MonoBehaviour
 {
-    public GameObject spawnpoint;
-    public GameObject mainPlayer;
-    private Rigidbody2D rb2;
+
+    public GameObject player;
+    public GameObject EnemyText;
+    public GameObject fadeOut;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            rb2 = mainPlayer.GetComponent<Rigidbody2D>();
-            rb2.velocity = new Vector2(0, 0); 
-            mainPlayer.transform.localPosition = spawnpoint.transform.localPosition;
+            player.GetComponent<RefinedMovement>().enabled = false;
+            EnemyText.SetActive(true);
+            fadeOut.SetActive(true);
+            StartCoroutine(RespawningLevel());
         }
     }
 
+    IEnumerator RespawningLevel()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(4);
+    }
 }
